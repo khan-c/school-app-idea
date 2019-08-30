d39e03203f34452d67ae2a8e252b6d40c4750c9c
Kyle Chen <kyle.h.chen@gmail.com>
2019-08-30 01:21:18 -0700

    Initial rest_framework set up

--- school_app/school_app/urls.py
+++ school_app/school_app/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('frontend.urls')),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- school_app/users/views.py
+++ school_app/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser
+from .serializers import UserSerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
